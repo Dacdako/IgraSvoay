c21f39bedec2fa8658f53142f332f0d6a06b8590
Dacdako <116169717+Dacdako@users.noreply.github.com>
2023-04-28 17:37:15 +0700

    Добавьте файлы проекта.

---
+++ IgraSvoay/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IgraSvoay
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        class Tabl1
+        {
+            string qests { get; set; }
+            string one { get; set; }
+            string two { get; set; }
+            string three { get; set; }
+            string fwo { get; set; }
+            string vife { get; set; }
+            public void tabl1()
+            { 
+
+            }
+
+        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
